4a69e8b3acacc41e79ed1edd9f6de0879826c88f
hiagoagtdev <hiagodevfe@gmail.com>
2026-07-25 13:15:00 -0300

    ios: AppDelegate repassa registro de push (APNs) ao Capacitor

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -46,4 +46,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 
+    // MARK: - Push Notifications (APNs)
+    // Repassa o retorno do registro de push para o plugin do Capacitor.
+    // Sem estes métodos, o token do APNs chega ao AppDelegate mas nunca vira
+    // o evento "registration"/"registrationError" no JS.
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: deviceToken)
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
+    }
+
 }
